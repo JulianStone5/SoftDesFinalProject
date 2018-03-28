1d11aad18a586045c7bbb0a9746284737be8f37d
JulianStone5 <julian.stone@students.olin.edu>
2018-03-28 11:34:06 -0400

    Made base code for initial steps

--- julian.py
+++ julian.py
@@ -1,1 +1,36 @@
 import pygame
+from pygame.locals import *
+import time
+import os
+
+class Map(object):
+
+    def __init__(self):
+
+class Player(object):
+
+    def __init__(self):
+
+class PyGameWindowView(object):
+
+    def __init__(self):
+
+
+class PyGameKeyboardController(object):
+
+    def __init__(self):
+
+
+if __name__ == '__main__':
+    pygame.init()
+
+    size = (1860,1020)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+        time.sleep(.001)
+
+    pygame.quit()
