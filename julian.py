c6714fbd3d5b384cb79829e3a7350368de815561
JulianStone5 <julian.stone@students.olin.edu>
2018-03-28 12:00:14 -0400

    Added more basic level code to julian.py

--- julian.py
+++ julian.py
@@ -5,32 +5,50 @@ import os
 
 class Map(object):
 
-    def __init__(self):
+    def __init__(self,size):
+        self.window_size = size
 
 class Player(object):
 
-    def __init__(self):
+    def __init__(self,height):
+        self.height = height
+
+class Model(object):
+
+    def __init__(self,size,player,mmap):
+        self.window_size = size
+        self.player = player
+        self.map = mmap
 
 class PyGameWindowView(object):
 
-    def __init__(self):
+    def __init__(self,size):
+        self.screen = pygame.display.set_mode(size)
+
+    def draw(self):
+        self.screen.fill(pygame.Color(0,0,0))
+
+        pygame.display.update()
 
 
 class PyGameKeyboardController(object):
 
-    def __init__(self):
-
+    def __init__(self,model):
+        self.model = model
 
 if __name__ == '__main__':
     pygame.init()
 
     size = (1860,1020)
 
+    view = PyGameWindowView(size)
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+        view.draw()
         time.sleep(.001)
 
     pygame.quit()
