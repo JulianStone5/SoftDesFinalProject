a31d88c33089163d6740c3f957937c864a49ada1
JulianStone5 <julian.stone@students.olin.edu>
2018-04-10 15:01:54 -0400

    COLLISION DETECTION WORKS HALLELUYAH

--- julian.py
+++ julian.py
@@ -8,27 +8,45 @@ class Map(pygame.sprite.Sprite):
     def __init__(self,size):
         self.size = size
         self.death_box = pygame.Rect(-1000,self.size[1],10000,50)
-        self.block1 = pygame.Rect(0,935,320,85)
-        self.blocks = [self.death_box,self.block1]
+        self.starter_block = pygame.Rect(0,935,320,85)
+        self.blocks = [self.death_box,self.starter_block]
         self.obstacles = []
-        self.add_block(255,255)
-        self.add_spring(self.blocks[2].x+100,self.blocks[2].y-30)
-        self.add_block(255,595)
+        self.add_small_block()
+        self.add_block()
         self.add_chasm()
-        self.add_block(600,595)
-        self.add_block(700,400)
-        self.add_roller(self.blocks[6].x+100,self.blocks[6].y-85)
-        self.add_block(600,595)
-        self.add_block(255,970)
+        self.add_block()
+        self.add_block()
+        self.add_small_block()
+        self.add_small_block()
+        self.add_block()
+        self.add_thin_block()
+        self.add_tall_block()
+        self.add_tall_block()
+        self.add_block()
+        self.add_block()
+        self.add_chasm()
+        self.add_chasm()
+        self.add_block()
+        self.add_thin_block()
+        self.add_block()
+        self.add_small_block()
+        self.add_smaller_block()
+        self.add_box()
+        self.add_box()
+        self.add_box()
+        self.add_box()
+        self.add_down_box()
+        self.add_down_box()
+
 
     def side_scroll(self,amount):
         for i in range(len(self.blocks)):
             self.blocks[i] = self.blocks[i].move(amount,0)
         for i in range(len(self.obstacles)):
-            #if self.obstacles[i].type != 'roller':
             self.obstacles[i].hit_box = self.obstacles[i].hit_box.move(amount,0)
 
-    def add_block(self,width=350,height=500):
+
+    def add_block(self,width=250,height=500):
         last_block = self.blocks[len(self.blocks)-1]
         x = last_block.x + last_block.w
         y = last_block.y + last_block.h - height
@@ -37,10 +55,47 @@ class Map(pygame.sprite.Sprite):
         block = pygame.Rect(x,y,width,height)
         self.blocks.append(block)
 
+    def add_tall_block(self,width=250,height=700):
+        self.add_block(width,height)
+
+    def add_thin_block(self, width=125, height=500):
+        self.add_block(width,height)
+
+    def add_small_block(self,width=250,height=250):
+        self.add_block(width,height)
+
+    def add_smaller_block(self,width=250,height=125):
+        self.add_block(width,height)
+
     def add_chasm(self,width=255,height=50):
         last_block = self.blocks[len(self.blocks)-1]
         x = last_block.x + last_block.w
         block = pygame.Rect(x,self.size[1]+50,width,height)
+        self.blocks.append(block)
+
+    def add_box(self,width=200,height=60):
+        last_block = self.blocks[-1]
+        x = last_block.x + last_block.w
+        y = last_block.y - 100 - height
+        if last_block.y > self.size[1]:
+            y = self.size[1]-height
+        block = pygame.Rect(x,y,width,height)
+        self.blocks.append(block)
+
+    def add_down_box(self,width=200,height=60):
+        last_block = self.blocks[-1]
+        x = last_block.x + last_block.w
+        y = last_block.y + 150 - height
+        if last_block.y > self.size[1]:
+            y = self.size[1]-height
+        block = pygame.Rect(x,y,width,height)
+        self.blocks.append(block)
+
+    def add_last_box(self,width=250,height=510):
+        last_block = self.blocks[-1]
+        x = last_block.x + last_block.w
+        y = 510
+        block = pygame.Rect(x,y,width,height)
         self.blocks.append(block)
 
     def add_spikes(self,x,y,width=85,height=30):
@@ -68,10 +123,24 @@ class Player(object):
                     self.height)
         self.gravity = .165
         self.vy = 8
-        self.vx = 5
+        self.vx = 4
         self.jump1 = False
         self.jump2 = False
         self.is_main = is_main
+
+    def top_collision(self,i,p1,p2,p3,p4):
+        if ((i.collidepoint(p1) and i.collidepoint(p2)) or # Collision on the top
+            (i.collidepoint(p3) and i.collidepoint(p4)) and self.vy < 0):
+            self.hit_box.y = i.y + i.h
+            self.vy = -1
+
+    def bottom_collision(self,i,p9,p10,p11,p12):
+        if ((i.collidepoint(p9) and i.collidepoint(p10)) or # Collision on the bottom
+            (i.collidepoint(p11) and i.collidepoint(p12)) and self.vy > 0):
+            self.hit_box.y = i.y - self.height
+            self.vy = 0
+            self.jump1 = False
+            self.jump2 = False
 
     def collision(self,mmap,game_over):
         """
@@ -82,7 +151,7 @@ class Player(object):
         -           -
         5           6
         -           -
-        -           -
+        13          14
         -           -
         7           8
         -           -
@@ -102,6 +171,9 @@ class Player(object):
         if self.vy > 10:
             self.vy = 10
         if game_over:
+            self.jump1 = True
+            self.jump2 = True
+            self.vx = 0
             return True
         # Make all key points
         p1 = self.hit_box.topleft
@@ -116,28 +188,26 @@ class Player(object):
         p8 = (p12[0],p12[1]-11)
         p10 = (p9[0]+11,p9[1])
         p11 = (p12[0]-11,p12[1])
+        p13 = (p5[0],p5[1]+50)
+        p14 = (p7[0],p7[1]-50)
+        p15 = (p6[0],p6[1]+50)
+        p16 = (p8[0],p8[1]-50)
         for a in range(len(mmap.blocks)):
             i = mmap.blocks[a]
             if self.hit_box.colliderect(i) and not game_over: # collision
                 if a == 0 and self.is_main:
                     return True
-                if ((i.collidepoint(p9) and i.collidepoint(p10)) or # Collision on the bottom
-                    (i.collidepoint(p11) and i.collidepoint(p12))):
-                    if self.vy > 0:
-                        self.hit_box.y = i.y - self.height
-                        self.vy = 0
-                        self.jump1 = False
-                        self.jump2 = False
-                if ((i.collidepoint(p1) and i.collidepoint(p2)) or # Collision on the top
-                    (i.collidepoint(p3) and i.collidepoint(p4))):
-                    self.hit_box.y = i.y + i.h
+                self.top_collision(i,p1,p2,p3,p4)
+                self.bottom_collision(i,p9,p10,p11,p12)
                 if ((i.collidepoint(p4) and i.collidepoint(p6)) or # Collision on the right
-                    (i.collidepoint(p8) and i.collidepoint(p12))):
+                    (i.collidepoint(p8) and i.collidepoint(p12)) or
+                    i.collidepoint(p15) or i.collidepoint(p16)):
                     self.hit_box.x = i.x - self.width
                     if not self.is_main:
                         self.vx *= -1
                 if ((i.collidepoint(p1) and i.collidepoint(p5)) or # Collision on the left
-                    (i.collidepoint(p7) and i.collidepoint(p9))):
+                    (i.collidepoint(p7) and i.collidepoint(p9)) or
+                    i.collidepoint(p13) or i.collidepoint(p14)):
                     self.hit_box.x = i.x + i.w
                     if not self.is_main:
                         self.vx *= -1
@@ -198,7 +268,8 @@ class PyGameWindowView(object):
             game_over_font = pygame.font.Font("freesansbold.ttf",50)
             game_over = game_over_font.render("GAME OVER",True,(255,0,0))
             restart = game_over_font.render('PRESS "SPACE" TO RESTART',True,(255,0,0))
-            self.screen.blit(game_over, (size[0]//2-game_over.get_width()//2, size[1]//2-game_over.get_height()))
+            self.screen.blit(game_over, (size[0]//2-game_over.get_width()//2,
+                                         size[1]//2-game_over.get_height()))
             self.screen.blit(restart, (size[0]//2-restart.get_width()//2, size[1]//2))
         pygame.display.update()
 
@@ -217,7 +288,7 @@ class PyGameKeyboardController(object):
             if self.model.player.hit_box.x < self.model.size[0]/4:
                 self.model.map.side_scroll(self.model.player.vx)
             else:
-                self.model.player.hit_box.x -= self.model.player.vx #change pos[0] to x because syntax
+                self.model.player.hit_box.x -= self.model.player.vx#change pos[0] to x because syntax
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             if self.model.player.hit_box.x > 3*self.model.size[0]/4:
                 self.model.map.side_scroll(-1*self.model.player.vx)
